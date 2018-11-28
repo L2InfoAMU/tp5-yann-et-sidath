57fab5c99d657f4d63094dc53833a12423de4318
m17005981 <yoshii13@yahoo.fr>
2018-11-28 11:00:54 +0100

    UPDATE: fichiers xml et iml créées

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -2,13 +2,21 @@ package image;
 
 import javafx.scene.paint.Color;
 
+import static util.Matrices.*;
+
 public class BruteRasterImage implements Image {
 
     private int width;
     private int height;
     private Pixel[][] pixels;
 
-
+    /**
+     * Creates a monochromatic image of given size.
+     *
+     * @param color the color of the image
+     * @param height the height of the image
+     * @param width the width of the image
+     */
     public BruteRasterImage(Color color, int width, int height){
         this.width = width;
         this.height = height;
@@ -19,8 +27,24 @@ public class BruteRasterImage implements Image {
         }
     }
 
-    public BruteRasterImage(Color[][] colors){
+    /**
+     * Creates an image from a given matrix.
+     * @param colors the matrix from which the image is created
+     * @throws IllegalArgumentException if the matrix is not rectangular or if it has an empty row or column
+     * @throws NullPointerException if the matrix has null parts
+     */
 
+    public BruteRasterImage(Color[][] colors){
+        requiresNonNull(colors);
+        requiresNonZeroDimensions(colors);
+        requiresRectangularMatrix(colors);
+        this.height = colors.length;
+        this.width = colors[0].length;
+        for(int x=0; x<this.getHeight(); x++){
+            for(int y=0; y<this.getWidth(); y++){
+                pixels[x][y]= new Pixel(x,y,colors[x][y]);
+            }
+        }
     }
 
     @Override
@@ -36,5 +60,21 @@ public class BruteRasterImage implements Image {
     @Override
     public int getHeight() {
         return this.height;
+    }
+
+    /**
+     * Allows to change the width of the image
+     * @param width the new width of the image
+     */
+    protected void setWidth(int width){
+        this.width= width;
+    }
+
+    /**
+     * Allows to change the height of the image.
+     * @param height the new height of the image
+     */
+    protected void setHeight(int height){
+        this.height= height;
     }
 }
