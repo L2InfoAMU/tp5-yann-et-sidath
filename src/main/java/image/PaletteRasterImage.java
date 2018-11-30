d9d4af5ac9c5795888c34a11949b09d4e8b78501
m17005981 <yoshii13@yahoo.fr>
2018-11-30 11:00:55 +0100

    UPDATE: .idea reconfig et début implémentation PaletteRasterImage

---
+++ src/main/java/image/PaletteRasterImage.java
@@ -0,0 +1,21 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+public class PaletteRasterImage implements Image {
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+}
