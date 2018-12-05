f38c4c6a18bb14f1b5bc339caaf99b7095aa60a5
m17005981 <yoshii13@yahoo.fr>
2018-12-05 11:33:01 +0100

    UPDATE: continuation de l'implémentation de SparseRasterImage

--- src/main/java/viewer/SparseRasterImage.java
+++ src/main/java/viewer/SparseRasterImage.java
@@ -1,21 +1,34 @@
 package viewer;
 
+import image.Point;
 import image.RasterImage;
 import javafx.scene.paint.Color;
 
+import java.util.HashMap;
+
+
 public class SparseRasterImage extends RasterImage {
+
+    HashMap<Point, Color> colors;
+
 
     public SparseRasterImage(Color color, int width, int height){
         super(width, height);
-
+        createRepresentation();
     }
 
     public  SparseRasterImage(Color[][] pixels){
         super(pixels);
+        createRepresentation();
+    }
+
+    public void createRepresentation(){
+        this.colors = new HashMap<>();
     }
 
     @Override
     public Color getPixelColor(int x, int y) {
         return null;
     }
+
 }
