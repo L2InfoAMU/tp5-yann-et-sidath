c9f62d48be2472e1ffca230ebfaf837e60d0cca2
m17005981 <yoshii13@yahoo.fr>
2018-12-05 10:48:40 +0100

    UPDATE: implémentation du setPixelColor, et des setPixelsColor, ainsi que leur documentation, et changement du nom du tableau de colors en pixels dans le second constructeur

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -14,16 +14,8 @@ public class PaletteRasterImage implements Image {
     int height;
 
     /**
-     * Allocate the matrix representing the image.
-     */
-
-    public void createRepresentation(){
-        this.colors = new ArrayList<>();
-        this.pixels = new int[this.getWidth()][this.getHeight()];
-    }
-
-    /**
      * Creates a monochromatic image of given size.
+     *
      * @param color the color of the image
      * @param width the width of the image
      * @param height the height of the image
@@ -33,38 +25,90 @@ public class PaletteRasterImage implements Image {
         this.width = width;
         this.height = height;
         createRepresentation();
-        colors.add(color);
+        this.colors.add(color);
         for (int x=0; x<this.getWidth(); x++){
             for (int y=0; y<this.getHeight(); y++){
-                pixels[x][y]= colors.indexOf(color);
+                this.pixels[x][y]= this.colors.indexOf(color);
             }
         }
     }
 
     /**
      * Creates an image from a matrix
-     * @param colors the matrix used to create the image
+     *
+     * @param pixels the matrix used to create the image
      */
-    public PaletteRasterImage(Color[][] colors){
-        requiresNonNull(colors);
-        requiresNonZeroDimensions(colors);
-        requiresRectangularMatrix(colors);
-        this.width = colors.length;
-        this.height = colors[0].length;
+    public PaletteRasterImage(Color[][] pixels){
+        requiresNonNull(pixels);
+        requiresNonZeroDimensions(pixels);
+        requiresRectangularMatrix(pixels);
+        this.width = pixels.length;
+        this.height = pixels[0].length;
         createRepresentation();
-        for (int x =0; x < this.getWidth(); x++){
+        for (int x = 0; x < this.getWidth(); x++){
             for (int y = 0; y< this.getHeight(); y++){
-                if (!this.colors.contains(colors[x][y])){
-                    this.colors.add(colors[x][y]);
+                if (!this.colors.contains(pixels[x][y])){
+                    this.colors.add(pixels[x][y]);
                 }
-                pixels[x][y] = this.colors.indexOf(colors[x][y]);
+                this.pixels[x][y] = this.colors.indexOf(pixels[x][y]);
             }
+        }
+    }
+
+    /**
+     * Allocate the matrix representing the image.
+     */
+
+    public void createRepresentation(){
+        this.colors = new ArrayList<>();
+        this.pixels = new int[this.getWidth()][this.getHeight()];
+    }
+
+    /**
+     * Allows to change the color of a pixel
+     *
+     * @param color the new color
+     * @param x,y the position of the pixel into the image
+     */
+    public void setPixelColor(Color color, int x, int y){
+        if (this.colors.contains(color)){
+            this.pixels[x][y] = this.colors.indexOf(color);
+        }
+        else{
+            this.colors.add(color);
+            this.pixels[x][y] = this.colors.indexOf(color);
         }
     }
 
     @Override
     public Color getPixelColor(int x, int y) {
         return colors.get(pixels[x][y]);
+    }
+
+    /**
+     * Allows to change the colors of the pixels according to a matrix
+     *
+     * @param pixels the new matrix to use for colors
+     */
+    public void setPixelsColor(Color[][] pixels){
+        for(int x = 0; x< this.getWidth(); x++){
+            for (int y = 0; y < this.getHeight(); y++){
+                this.setPixelColor(pixels[x][y], x, y);
+            }
+        }
+    }
+
+    /**
+     * Allows to change the image into a monochromatic image
+     *
+     * @param color the color of the new monochromatic image
+     */
+    private void setPixelsColor(Color color){
+        for(int x = 0; x< this.getWidth(); x++){
+            for (int y = 0; y < this.getHeight(); y++){
+                this.setPixelColor(color, x, y);
+            }
+        }
     }
 
     @Override
